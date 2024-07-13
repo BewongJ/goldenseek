885b007d8c94593f1aa3be3f6c485636fd66e870
Bewong <133567584+BewongJ@users.noreply.github.com>
2024-07-13 18:16:08 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -15,31 +15,17 @@ function App() {
 
   return (
     <div className="bg-black min-h-screen">
-      <div className="navbar bg-white-500 w-100 h-10 text-center text-white font-mono text-2xl">
-        GOLDEN SEEKS
-      </div>
-      <div className="flex justify-between items-center mt-4 px-10">
+      <div className="">
         <button
           onClick={toggleModel1}
-          className="bg-yellow-500 text-white font-bold py-2 px-4 rounded"
+          className="flex bg-white text-black font-bold justify-center items-center py-2 px-4 rounded"
         >
           {activeModel === 'model1' ? 'Hide Model' : 'Candy Top'}
-        </button>
-        <button
-          onClick={toggleModel2}
-          className="bg-yellow-500 text-white font-bold py-2 px-4 rounded"
-        >
-          {activeModel === 'model2' ? 'Hide Model' : 'Test'}
         </button>
       </div>
       {activeModel === 'model1' && (
         <div className="mt-4">
           <Models1 />
-        </div>
-      )}
-      {activeModel === 'model2' && (
-        <div className="mt-4">
-          <Models2 />
         </div>
       )}
     </div>
