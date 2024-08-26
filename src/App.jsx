3e52a4bc8d670ba2be051453f75ec17b5fdeefb3
Bewong <133567584+BewongJ@users.noreply.github.com>
2024-08-26 22:06:02 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,14 @@
-import React, { useState } from 'react';
-import Models1 from './components/Models1';
-import Models2 from './components/Models2';
-import { FaEyeSlash } from 'react-icons/fa';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
-  const [activeModel, setActiveModel] = useState(null);
-
-  const toggleModel1 = () => {
-    setActiveModel(activeModel === 'model1' ? null : 'model1');
-  };
-
-  const toggleModel2 = () => {
-    setActiveModel(activeModel === 'model2' ? null : 'model2');
-  };
-
   return (
-    <div className="bg-black min-h-screen">
-      <br></br>
-      <div className="flex justify-between items-center px-10 mx-0">
-        <button
-          onClick={toggleModel1}
-          className="bg-white text-black font-bold py-2 px-4 rounded"
-        >
-          {activeModel === 'model1' ? <FaEyeSlash /> : 'Candy Top'}
-        </button>
-        {/* <button
-          onClick={toggleModel2}
-          className="bg-white text-black font-bold py-2 px-4 rounded"
-        >
-          {activeModel === 'model2' ? <FaEyeSlash /> : 'GDS Polo'}
-        </button> */}
-      </div>
-      {activeModel === 'model1' && (
-        <div className="mt-4">
-          <Models1 />
-        </div>
-      )}
-      {activeModel === 'model2' && (
-        <div className="mt-4">
-          <Models2 />
-        </div>
-      )}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
